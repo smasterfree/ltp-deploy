55d83c75085a838d5e3d5883a7f4cce29f5b3814
hzhuangzhexiao <hzhuangzhexiao>
2019-02-19 09:33:06 +0800

    first commit

---
+++ deploy-ltp-new.sh
@@ -0,0 +1,16 @@
+apt-get install  htop  fping supervisor xz-utils  -y
+wget  -O /opt/ltp-20190115.tar.xz https://fat.nos-eastchina1.126.net/ltp-20190115.tar.xz
+cd /opt
+tar -xf  /opt/ltp*.tar.xz
+
+echo " /opt/ltp/runltp -c 4 -i 2 -m 2,4,10240,1 -D 2,10,10240,1 -p -q  -l /tmp/result-log.27351 -o /tmp/result-output.27351 -C /tmp/result-failed.27351 -d /tmp   -t 30d "  > /root/ltp.sh
+
+cat <<EOT >> /etc/supervisor/supervisord.conf
+[program:ltp]
+command=sh /root/ltp.sh
+autorestart=true
+user=root
+EOT
+
+sleep 3
+supervisorctl reload
