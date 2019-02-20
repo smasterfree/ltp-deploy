858e105f07cf36b8875ce2038d8f286ee1a75a0e
hzhuangzhexiao <hzhuangzhexiao>
2019-02-20 10:33:17 +0800

    update, add nq

--- deploy-ltp-new.sh
+++ deploy-ltp-new.sh
@@ -2,18 +2,31 @@
 apt-get install  htop  fping supervisor xz-utils  -y
 
 
+# get ltp binary
 wget  -O /opt/ltp-20190115.tar.xz https://fat.nos-eastchina1.126.net/ltp-20190115.tar.xz
+
+cd /opt
 tar -xf  /opt/ltp*.tar.xz
 
+# skip file 
+wget  -O /opt/ltp/skip_file  https://raw.githubusercontent.com/smasterfree/ltp-deploy/master/skip_file
 
-echo " /opt/ltp/runltp -c 4 -i 2 -m 2,4,10240,1 -D 2,10,10240,1 -p -q  -l /tmp/result-log.27351 -o /tmp/result-output.27351 -C /tmp/result-failed.27351 -d /tmp  -t 30d "  > /root/ltp.sh
+# nq to run background
+wget  -O /bin/nq  https://raw.githubusercontent.com/smasterfree/ltp-deploy/master/nq
+chmod 777 /bin/nq
 
-cat <<EOT >> /etc/supervisor/supervisord.conf
-[program:ltp]
-command=sh /root/ltp.sh
-autorestart=true
-user=root
-EOT
 
-sleep 3
-supervisorctl reload
+nq  /opt/ltp/runltp -c 4 -i 2 -m 2,4,10240,1 -D 2,10,10240,1 -p -q -S /opt/ltp/skip_file  -l /tmp/result-log.27351 -o /tmp/result-output.27351 -C /tmp/result-failed.27351 -d /tmp  -t 30d
+
+
+#echo " /opt/ltp/runltp -c 4 -i 2 -m 2,4,10240,1 -D 2,10,10240,1 -p -q -S /opt/ltp/skip_file  -l /tmp/result-log.27351 -o /tmp/result-output.27351 -C /tmp/result-failed.27351 -d /tmp  -t 30d "  > /root/ltp.sh
+
+##cat <<EOT >> /etc/supervisor/supervisord.conf
+##[program:ltp]
+##command=sh /root/ltp.sh
+##autorestart=true
+##user=root
+##EOT
+
+##sleep 3
+##supervisorctl reload
