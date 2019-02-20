da40b4dddc7a046acfca039e469a46d4f3f44cae
hzhuangzhexiao <hzhuangzhexiao>
2019-02-20 15:53:30 +0800

    update old version ltp, use /root path

--- deploy-ltp-old.sh
+++ deploy-ltp-old.sh
@@ -3,16 +3,16 @@
 apt-get install xz-utils  -y
 
 # get ltp binary
-rm -rf  /opt/ltp-bin2.tar.gz
-wget   --no-check-certificate  -O /opt/ltp-bin2.tar.gz   https://fat.nos-eastchina1.126.net/ltp-bin2.tar.gz
-cd /opt
-tar -xf  /opt/ltp-bin2.tar.gz
+rm -rf  /root/ltp-bin2.tar.gz
+wget   --no-check-certificate  -O /root/ltp-bin2.tar.gz   https://fat.nos-eastchina1.126.net/ltp-bin2.tar.gz
+cd /root
+tar -xf  /root/ltp*.tar.gz
 
 
 # nq to run background
 wget  -O /bin/nq  https://fat.nos-eastchina1.126.net/nq
 chmod 777 /bin/nq
 
-nq  /opt/ltp/testscripts/ltpstress.sh  -d /tmp   -t 1000  -n -p -l /tmp/ltplog.12431
-
+cd /root/ltp/testscripts/
+nq  ./ltpstress.sh  -d /tmp   -t 1000  -n -p -l /tmp/ltplog.12431
 
