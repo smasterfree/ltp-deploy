688863f02633e52ebf7bb56479f9c8b64281026b
hzhuangzhexiao <hzhuangzhexiao>
2019-02-20 11:01:08 +0800

    rm previous supervisor conf

--- deploy-ltp-new.sh
+++ deploy-ltp-new.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get install  htop  fping supervisor xz-utils  -y
 
+apt-get install htop fping supervisor xz-utils  -y
 
 # get ltp binary
 rm -rf  ltp-20190115.tar.xz
@@ -16,18 +16,7 @@ wget  -O /opt/ltp/skip_file  https://raw.githubusercontent.com/smasterfree/ltp-d
 wget  -O /bin/nq  https://raw.githubusercontent.com/smasterfree/ltp-deploy/master/nq
 chmod 777 /bin/nq
 
-
 nq  /opt/ltp/runltp -c 4 -i 2 -m 2,4,10240,1 -D 2,10,10240,1 -p -q -S /opt/ltp/skip_file  -l /tmp/result-log.27351 -o /tmp/result-output.27351 -C /tmp/result-failed.27351 -d /tmp  -t 30d
 
 
-#echo " /opt/ltp/runltp -c 4 -i 2 -m 2,4,10240,1 -D 2,10,10240,1 -p -q -S /opt/ltp/skip_file  -l /tmp/result-log.27351 -o /tmp/result-output.27351 -C /tmp/result-failed.27351 -d /tmp  -t 30d "  > /root/ltp.sh
 
-##cat <<EOT >> /etc/supervisor/supervisord.conf
-##[program:ltp]
-##command=sh /root/ltp.sh
-##autorestart=true
-##user=root
-##EOT
-
-##sleep 3
-##supervisorctl reload
