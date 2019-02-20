c29478459f45072aa71e0aae0ea2bd0d441ee72b
hzhuangzhexiao <hzhuangzhexiao>
2019-02-20 15:41:24 +0800

    old version bug fix

--- deploy-ltp-old.sh
+++ deploy-ltp-old.sh
@@ -6,13 +6,13 @@ apt-get install xz-utils  -y
 rm -rf  /opt/ltp-bin2.tar.gz
 wget   --no-check-certificate  -O /opt/ltp-bin2.tar.gz   https://fat.nos-eastchina1.126.net/ltp-bin2.tar.gz
 cd /opt
-tar -xf  /opt/ltp*.tar.xz
+tar -xf  /opt/ltp-bin2.tar.gz
 
 
 # nq to run background
 wget  -O /bin/nq  https://fat.nos-eastchina1.126.net/nq
 chmod 777 /bin/nq
 
-nq  /root/ltp/testscripts/ltpstress.sh  -d /tmp   -t 1000  -n -p -l /tmp/ltplog.12431
+nq  /opt/ltp/testscripts/ltpstress.sh  -d /tmp   -t 1000  -n -p -l /tmp/ltplog.12431
 
 
