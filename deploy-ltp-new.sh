0813ff50a11d8d1a0a19da2f22c8e8ba0474e86e
hzhuangzhexiao <hzhuangzhexiao>
2019-02-20 13:06:25 +0800

    update nq to nos

--- deploy-ltp-new.sh
+++ deploy-ltp-new.sh
@@ -13,7 +13,7 @@ tar -xf  /opt/ltp*.tar.xz
 wget  -O /opt/ltp/skip_file  https://raw.githubusercontent.com/smasterfree/ltp-deploy/master/skip_file
 
 # nq to run background
-wget  -O /bin/nq  https://raw.githubusercontent.com/smasterfree/ltp-deploy/master/nq
+wget  -O /bin/nq  https://fat.nos-eastchina1.126.net/nq
 chmod 777 /bin/nq
 
 nq  /opt/ltp/runltp -c 4 -i 2 -m 2,4,10240,1 -D 2,10,10240,1 -p -q -S /opt/ltp/skip_file  -l /mnt/result-log.27351 -o /mnt/result-output.27351 -C /mnt/result-failed.27351 -d /tmp  -t 30d
